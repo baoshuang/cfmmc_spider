ce135e3d1eff9a5ce78471f7a5f1be6640cbd550
shifulin <s.flin@foxmail.com>
2019-05-25 16:44:51 +0800

    初始创建

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # cfmmc_spider
 
-自动识别验证码登录[期货保证金监控中心](https://investorservice.cfmmc.com/)
-爬取数据
+###自动识别验证码登录[期货保证金监控中心](https://investorservice.cfmmc.com/)爬取数据
+
+
+使用tesseract识别码
+
+安装tesseract-ocr环境
+```
+sudo apt-get install g++
+sudo apt-get install autoconf automake libtool
+sudo apt-get install pkg-config
+sudo apt-get install libpng-dev
+sudo apt-get install libjpeg8-dev
+sudo apt-get install libtiff5-dev
+sudo apt-get install zlib1g-dev
+git clone git@github.com:DanBloomberg/leptonica.git
+cd leptonica
+./autogen.sh
+./configure
+make
+make install
+sudo apt-get install tesseract-ocr
+```
+安装pytesseract
+```
+pip install pytesseract
+```
---
+++ cfmmc.py
@@ -0,0 +1,80 @@
+import time
+from io import BytesIO
+
+from requests import session
+from PIL import Image
+import PIL.ImageOps
+import pytesseract
+
+
+def reg_img(file_obj):
+    im = Image.open(file_obj)
+    im = im.convert('L')
+    binary_image = im.point([0 if i < 210 else 1 for i in range(256)], '1')
+    im1 = binary_image.convert('L')
+    im2 = PIL.ImageOps.invert(im1)
+    im3 = im2.convert('1')
+    im4 = im3.convert('L')
+    res = pytesseract.image_to_string(im4)
+    # print(res)
+    # im4.show()
+    return res
+
+
+def flag_filter(content, flag):
+    return content[content.find(flag) + len(flag):].split('"')[0]
+
+
+def get_current_equity(content):
+    a = content[content.find('客户权益'):]
+    b = a.split('<td class="table-normal-text" align="right">')[1]
+    c = float(b[:b.find('&nbsp;')].strip())
+    return c
+
+
+def do(user_id="***********", passwd='***********'):
+    header = {
+        'Connection': 'keep-alive',
+        'User-Agent': "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
+    }
+    url = "https://investorservice.cfmmc.com/login.do"
+    token_flag = 'name="org.apache.struts.taglib.html.TOKEN" value="'
+    veri_code_flag = '<img id="imgVeriCode" src="'
+    ss = session()
+    res = ss.get(url, headers=header)
+    content = res.content.decode()
+    token = flag_filter(content, token_flag)
+    veri_code_url = 'https://investorservice.cfmmc.com' + flag_filter(content, veri_code_flag)
+    for i in range(200):
+        print(i)
+        try:
+            tmp_file = BytesIO()
+            tmp_file.write(ss.get(veri_code_url).content)
+            veri_code = reg_img(tmp_file)
+            if veri_code and len(veri_code) == 6:
+                veri_code = ''.join(filter(str.isalnum, veri_code))
+                print(veri_code)
+                post_data = {
+                    "org.apache.struts.taglib.html.TOKEN": token,
+                    "showSaveCookies": '',
+                    "userID": user_id,
+                    "password": passwd,
+                    "vericode": veri_code,
+                }
+                content2 = ss.post(url, data=post_data, headers=header, timeout=5)
+                res = content2.content.decode()
+                if "验证码错误" not in res:
+                    # print('页面:', res)
+                    current_equity = get_current_equity(res)
+                    print('爬取客户权益 成功')
+                    print(user_id, current_equity)
+                    return
+            time.sleep(1)
+            veri_code_url = "https://investorservice.cfmmc.com/veriCode.do?t=" + str(int(time.time() * 1000))
+        except Exception as e:
+            print(e)
+    print('爬取客户权益 失败')
+
+
+if __name__ == '__main__':
+    do()
